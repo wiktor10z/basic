399be0450dbaaa180d821cd23343a89a8452053e
wiktor10z <wiktor10z@gmail.com>
2015-12-27 13:26:34 +0000

    kod4 - numery i zliczanie zmiennych

--- MRJP/wz320501_latc/src/Latc_compiler.hs
+++ MRJP/wz320501_latc/src/Latc_compiler.hs
@@ -22,6 +22,7 @@ type BlockGraph = Map.Map Integer Code4Block
 
 
 data Code4Block = Label [Code4Instruction] [Code4Block] [Code4Block]
+
 data Code4Instruction =
 	Empty4 ValVar4 |
 	Ass4 ValVar4 ValVar4|
@@ -47,26 +48,34 @@ data ValVar4 =
 	Bool4 Bool |
 	String4 String |
 	Temp4 Integer |
-	Var4 String	|				--może też var integer - jednoznaczne nazwy, ale trzeba też pamiętać w jakimś stanie, aby rozróżniać który który oraz w envie który aktualny
+	Var4 Integer |				--może też var integer - jednoznaczne nazwy, ale trzeba też pamiętać w jakimś stanie, aby rozróżniać który który oraz w envie który aktualny
 	Void4
     deriving (Eq,Ord,Show)	
 
-type Temps4 = State Integer
+type Env4 = Map.Map String Integer
+
+type Vars4 = ReaderT Env4 (State (Integer,Integer))
 
 
-nextTemp :: Temps4 Integer
+--TODOTODO generowanie zmiennych temporalnych + labeli  - Stan - dla labeli tak, dla wyrażenia jak drzewo nie płaskie - wielokrotne rozgałęzienia? 
+ 
+nextTemp :: Vars4 Integer
 nextTemp = do
-	temps <- get
-	put (temps+1)
+	(vars,temps) <- get
+	put (vars,temps+1)
 	return temps
 
-type Vars4 = ReaderT (Map.Map String Integer) (State Integer)
+newVar :: Vars4 Integer
+newVar = do
+	(vars,temps) <- get
+	put (vars+1,temps)
+	return vars
 
 
---TODOTODO generowanie zmiennych temporalnych + labeli  - Stan - dla labeli tak, dla wyrażenia jak drzewo nie płaskie - wielokrotne rozgałęzienia?  
 
 
-toCode4Expr :: Expr -> Temps4 ([Code4Instruction],ValVar4)
+
+toCode4Expr :: Expr -> Vars4 ([Code4Instruction],ValVar4)
 
 toCode4Expr (EAdd exp1 (Plus (PPlus _)) exp2) = do					--dla apply param x ... call f , n
 	(str1,x1) <- toCode4Expr exp1
@@ -81,49 +90,71 @@ toCode4Expr (EAdd exp1 (Minus (PMinus _)) exp2) = do
 	return (str1++str2++[OpV Sub4 (Temp4 temp) x1 x2],(Temp4 temp))
 	--TODO reszta operatorów - można później - to tylko praca odtwórcza, te powinny wystarczyć do celów programowania
 	
-toCode4Expr (EVar (PIdent (_,x))) = return ([],Var4 x)
+toCode4Expr (EVar (PIdent (_,x))) = do
+	env <- ask 
+	case Map.lookup x env of
+		Just varnum -> return ([],Var4 varnum)
 toCode4Expr (ELitInt n) = return ([],Int4 n)
 toCode4Expr ELitTrue = return ([],Bool4 True)
 toCode4Expr ELitFalse = return ([],Bool4 False)
 toCode4Expr (EString str) = return ([],String4 str)
 
 
-toCode4Ass :: String -> Code4Instruction -> Code4Instruction
-toCode4Ass varname (OpV op (Temp4 _) x1 x2) = (OpV op (Var4 varname) x1 x2)
-toCode4Ass varname (Empty4 x) = Ass4 (Var4 varname) x
+toCode4Ass :: Integer -> Code4Instruction -> Code4Instruction
+toCode4Ass varnum (OpV op (Temp4 _) x1 x2) = (OpV op (Var4 varnum) x1 x2)
+toCode4Ass varnum (Empty4 x) = Ass4 (Var4 varnum) x
 
 
-toCode4Decl :: Type -> [Item] -> [Code4Instruction]		--trzeba by dodać jakieś zliczanie zmiennych lokalnych - dla deklaracji przesunięcia w funkcji (alokacji)
-toCode4Decl t ((Init (PIdent (_,varname)) exp):its) =
-	let ((r,temp),_) = runState (toCode4Expr exp) 0
-	in if (null r)
-		then [toCode4Ass varname (Empty4 temp)]
-		else (init r) ++[toCode4Ass varname (last r)]
+toCode4Decl :: Type -> [Item] -> Vars4 (Env4,[Code4Instruction])
+toCode4Decl t ((Init (PIdent (_,varname)) exp):its) = do
+	(r,temp) <- toCode4Expr exp
+	varnum <- newVar
+	(env4,r2) <- (local (Map.insert varname varnum)  (toCode4Decl t its))
+	if (null r)
+		then return (env4,[toCode4Ass varnum (Empty4 temp)]++r2)
+		else return (env4,(init r) ++[toCode4Ass varnum (last r)]++r2)
+
+toCode4Decl _ [] = do
+	env <- ask
+	return (env,[])
 
 
-toCode4Stmt :: Stmt -> [Code4Instruction]
 
-toCode4Stmt Empty = []
+--TODO NInit - tutaj albo usunąć wcześniej - we frontendzie
 
-toCode4Stmt (BStmt (Block bl)) = toCode4Block bl
+toCode4Stmt :: Stmt -> Vars4 (Env4,[Code4Instruction])
+
+toCode4Stmt Empty = do
+	env <- ask
+	return (env,[])
+
+toCode4Stmt (BStmt (Block bl)) = do
+	env <- ask
+	r <- toCode4Block bl
+	return (env,r)
 
 toCode4Stmt (Decl t l) = toCode4Decl t l
 
-toCode4Stmt (Ass (PIdent (_,varname)) exp) =
-	let ((r,temp),_) = runState (toCode4Expr exp) 0
-	in if (null r)
-		then [toCode4Ass varname (Empty4 temp)]
-		else (init r) ++[toCode4Ass varname (last r)]
+toCode4Stmt (Ass (PIdent (_,varname)) exp) = do
+	env <- ask
+	(r,temp) <- toCode4Expr exp
+	case Map.lookup varname env of
+		Just varnum -> if (null r)
+				then return (env,[toCode4Ass varnum (Empty4 temp)])
+				else return (env,(init r) ++[toCode4Ass varnum (last r)])
 
-toCode4Stmt (Incr (PIdent (_,varname))) = [] --TODO
+--toCode4Stmt (Incr (PIdent (_,varname))) = return [] --TODO
 
-toCode4Stmt (Decr (PIdent (_,varname))) = [] --TODO
+--toCode4Stmt (Decr (PIdent (_,varname))) = return [] --TODO
 
-toCode4Stmt (Ret _ exp) =
-	let ((r,temp),_) = runState (toCode4Expr exp) 0
-	in (r ++[Return4 temp])
+toCode4Stmt (Ret _ exp) = do
+	env <- ask
+	(r,temp) <-toCode4Expr exp
+	return  (env,r ++[Return4 temp])
 
-toCode4Stmt (VRet _) = [Return4 Void4]
+toCode4Stmt (VRet _) = do
+	env <- ask
+	return (env,[Return4 Void4])
 
 --TODOTODO conditionale
 
@@ -134,18 +165,22 @@ toCode4Stmt (VRet _) = [Return4 Void4]
 
 
 
-toCode4Stmt (SExp exp) =		--TODO można zamienić OpV na OpE
-	let ((r,_),_) = runState (toCode4Expr exp) 0
-	in r
+toCode4Stmt (SExp exp) = do		--TODO można zamienić OpV na OpE
+	env <- ask
+	(r,_) <- toCode4Expr exp
+	return (env,r)
 
 
 
-toCode4Block :: [Stmt] -> [Code4Instruction]		--TODO inne
-toCode4Block (stm:stmts) = (toCode4Stmt stm)++(toCode4Block stmts)
-toCode4Block [] = []
+toCode4Block :: [Stmt] -> Vars4 [Code4Instruction]		--TODO inne
+toCode4Block (stm:stmts) = do
+	(env4,a1) <- toCode4Stmt stm
+	a2 <- (local (\x -> env4) (toCode4Block stmts))
+	return (a1++a2)
+toCode4Block [] = return []
 
 
-toCode4 :: TopDef -> [Code4Instruction]			--TODO inne			--TODO powinno zapamiętać liczbę zmiennych lokalnych, żeby wiedzieć o ile przesunąć stos
+toCode4 :: TopDef -> Vars4 [Code4Instruction]			--TODO inne			--TODO powinno zapamiętać liczbę zmiennych lokalnych, żeby wiedzieć o ile przesunąć stos
 toCode4 (FnDef _ _ _ (Block bl)) = toCode4Block bl
 
 
@@ -157,7 +192,7 @@ compileFunctions ::[TopDef] ->StEnv ()
 
 compileFunctions (f:fs) = do
 	--compileFunction f
-	let code42 = toCode4 f	
+	let (code42,_) = runState (runReaderT (toCode4 f) (Map.empty)) (0,0)	--TODO zamiast empty argumenty
 	error (show code42)
 	return ()
 	
